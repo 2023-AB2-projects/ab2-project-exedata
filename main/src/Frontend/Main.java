6e83cc7784c112da7a75ad8e055b2ce2d7af6417
Kerekes Balazs-Zoltan <kerekesbaly@gmail.com>
2023-03-14 01:31:42 +0200

    I created the basic of the frontend!

--- main/src/Frontend/Main.java
+++ main/src/Frontend/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // Create a swing frame and start the application
         // Balazs git teszt
+        View view=new View();
     }
 }
