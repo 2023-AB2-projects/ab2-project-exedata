64ed762b4965638d09b1247e0a9828589a9cc3bb
Benko Botond <benkobotika@gmail.com>
2023-03-18 00:35:59 +0200

    Backend working: server-client connection very good, JSON file created in backend

--- main/src/Backend/Backend.java
+++ main/src/Backend/Backend.java
@@ -4,8 +4,6 @@ import Backend.HttpServer.Server;
 import Backend.SaveLoadJSON.LoadJSON;
 import Backend.SaveLoadJSON.SaveJSON;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
-
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
@@ -13,15 +11,11 @@ import java.io.IOException;
 public class Backend {
     public static void main(String[] args) throws IOException {
         Server server = new Server(12000);
-//        try {
-//            Parser.commandType("CREATE DATABASE PERSONS;").performAction(); //create a new database
-//        } catch (UnknownCommandException | ParserConfigurationException | TransformerException e) {
-//            throw new RuntimeException(e);
-//        }
-//        JSONObject a = new JSONObject();
-        //SaveJSON.save(a, "databases.json");
-        //JSONObject b = LoadJSON.load("databases.json");
-        //System.out.println(b);
+        try {
+            Parser.commandType("CREATE DATABASE PERSONS;").performAction(); //create a new database
+        } catch (UnknownCommandException | ParserConfigurationException | TransformerException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
