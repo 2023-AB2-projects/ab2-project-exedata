de7fae99ea5673ee72e3c5644fe97b61c6887cfc
Benko Botond <benkobotika@gmail.com>
2023-03-17 17:01:27 +0200

    nothing big

--- main/src/Backend/SaveLoadJSON/LoadJSON.java
+++ main/src/Backend/SaveLoadJSON/LoadJSON.java
@@ -1,5 +1,6 @@
 package Backend.SaveLoadJSON;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
