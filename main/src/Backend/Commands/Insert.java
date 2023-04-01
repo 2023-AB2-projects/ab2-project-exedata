f8966e1919175a74781f0008350894ae1640d143
Benko Botond <benkobotika@gmail.com>
2023-04-01 23:01:26 +0300

    Insert to MongoDB good format, just I need to add Null if value doesn t exist

--- main/src/Backend/Commands/Insert.java
+++ main/src/Backend/Commands/Insert.java
@@ -10,7 +10,9 @@ import org.bson.Document;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,6 +20,7 @@ import java.util.regex.Pattern;
 public class Insert implements Command {
     private final String command;
     private MongoDB mongoDB;
+    private List<String> primaryKeys;
 
     public Insert(String command) {
         this.command = command;
@@ -36,25 +39,35 @@ public class Insert implements Command {
             String[] value = matcher.group(3).replaceAll("\\s+", "").split(",");
 
             // if(insertValidation(tableName,fieldName,value)) {
-            String insertValue = "";
-            for(int i=0; i<fieldName.length; i++) {
-                if (value[i].charAt(0) == '\"') {
-                    insertValue = insertValue.concat(value[i].substring(1, value[i].length()-1) + "#");
-                } else {
-                    insertValue = insertValue.concat(value[i] + "#");
-                }
-            }
-            insertValue = insertValue.substring(0, insertValue.length()-1);
 
             Parser.currentDatabaseName = "University";
             if (Parser.currentDatabaseName == null) {
                 System.out.println("Please select your database first!");
             } else {
-                List<String> list = getPrimaryKeys(Parser.currentDatabaseName, tableName);
-                Document document = new Document();
-                int freeID = 100; // here should be primary key, foreign key, ...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                primaryKeys = getPrimaryKeys(Parser.currentDatabaseName, tableName);
+                String primaryKeysString = "";
+                for (String s : fieldName) {
+                    for (int i=0; i<primaryKeys.size(); i++) {
+                        if (Objects.equals(primaryKeys.get(i), s)) {
+                            primaryKeysString = primaryKeysString.concat(value[i] + "#");
+                        }
+                    }
+                }
+                primaryKeysString = primaryKeysString.substring(0, primaryKeysString.length()-1);
 
-                document.append("_id", String.valueOf(freeID));
+                String insertValue = "";
+                for(int i=0; i<fieldName.length; i++) {
+                    if (value[i].charAt(0) == '\"') {
+                        value[i] = value[i].substring(1, value[i].length()-1);
+                    }
+                    if (!isPrimaryKey(fieldName[i])) {
+                        insertValue = insertValue.concat(value[i] + "#");
+                    }
+                }
+                insertValue = insertValue.substring(0, insertValue.length()-1);
+
+                Document document = new Document();
+                document.append("_id", primaryKeysString);
                 document.append("Value", insertValue);
                 mongoDB.createDatabaseOrUse(Parser.currentDatabaseName);
                 mongoDB.insertOne(tableName, document);
@@ -70,5 +83,14 @@ public class Insert implements Command {
         Databases databases = LoadJSON.load("databases.json");
         assert databases != null;
         return databases.getDatabase(dataBaseName).getTable(tableName).getPrimaryKey();
+    }
+
+    public boolean isPrimaryKey(String fieldName) {
+        for (String primaryKey : primaryKeys) {
+            if (primaryKey.equals(fieldName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- main/src/Backend/Commands/InsertTestMain.java
+++ main/src/Backend/Commands/InsertTestMain.java
@@ -5,7 +5,7 @@ import javax.xml.transform.TransformerException;
 
 public class InsertTestMain {
     public static void main(String[] args) throws ParserConfigurationException, TransformerException {
-        Command insert = new Insert("INSERT INTO disciplines (Name, Salary) VALUES (\"Jozsef\", 5000);");
+        Command insert = new Insert("INSERT INTO disciplines (DiscID, Name, Salary) VALUES (11, \"Jozsef\", 5000);");
         insert.performAction();
     }
 }
