ee5a95ebe6d643ee6df07d125f21402e4c9c29f7
Benko Botond <benkobotika@gmail.com>
2023-04-01 15:30:45 +0300

    I created MongoDB connection and some methods: connectToLocalhost, disconnectFromLocalhost, createDatabaseOrUse, dropDatabase, createCollection, dropCollection, insertOne, deleteOne

---
+++ main/src/MongoDBManagement/MongoTest.java
@@ -0,0 +1,20 @@
+package MongoDBManagement;
+import org.bson.Document;
+
+public class MongoTest {
+    public static void main(String[] args) {
+        MongoDB mongoDB = new MongoDB();
+        mongoDB.connectToLocalhost();
+
+//        mongoDB.useOrCreateDatabase("new");
+//        Document document= new Document();
+//        document.append("ID", "1");
+//        document.append("Name", "John Smith");
+//        document.append("Age", "45");
+//        document.append("Location", "New York");
+//        //mongoDB.insertOne("new", document);
+//        mongoDB.deleteOne("new", "ID", "1");
+
+
+    }
+}
