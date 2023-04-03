0ab295be9fbed79f0e5df65533c285ab253c97f5
Benko Botond <benkobotika@gmail.com>
2023-04-03 23:57:33 +0300

    PanelDown design

--- main/src/Frontend/PanelDown.java
+++ main/src/Frontend/PanelDown.java
@@ -1,12 +1,15 @@
 package Frontend;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PanelDown extends JPanel {
     private JLabel errorLabel;
 
     public PanelDown() {
         errorLabel = new JLabel("Error chanel!");
+        errorLabel.setForeground(Color.red);
+        errorLabel.setFont(new Font("Berlin Sans FB", Font.BOLD, 20));
         this.add(errorLabel);
     }
 
