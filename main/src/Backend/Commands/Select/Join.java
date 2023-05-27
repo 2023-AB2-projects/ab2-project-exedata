9a912f009050ec6d10582035b1e3dc85d6bace9c
Benko Botond <benkobotika@gmail.com>
2023-05-28 02:44:46 +0300

    Join prepared

--- main/src/Backend/Commands/Select/Join.java
+++ main/src/Backend/Commands/Select/Join.java
@@ -14,8 +14,9 @@ public class Join {
     private SelectManager selectManager;
     private List<String> tableNames;
     private Selection tableSelectResultsSeparately;
+    private List<List<String>> tableSelectResults;
     private List<Condition> joinConditions;
-    private List<List<String>> indexArray; // attributeNames of a table (ex. indexArray.get(0) => [DiscID, DName, CreditNr])
+    private List<List<String>> attributeNamesTableIndex; // attributeNames of a table (ex. indexArray.get(0) => [DiscID, DName, CreditNr])
     private List<String> joinResult;
 
     public Join(SelectManager selectManager) {
@@ -23,26 +24,24 @@ public class Join {
         tableNames = selectManager.getFrom();
         tableSelectResultsSeparately = new Selection(selectManager);
         joinConditions = selectManager.getJoin();
-        indexArray = new ArrayList<>();
+        attributeNamesTableIndex = new ArrayList<>();
+        tableSelectResults = new ArrayList<>();
 
+        List<String> allAttributeNames = new ArrayList<>();
+        for (int i=0; i<tableNames.size(); i++) {
+            List<String> selectResults = tableSelectResultsSeparately.processing(i);
+            List<String> attributeNames = getAttributeNames(tableNames.get(i));
+            allAttributeNames.addAll(attributeNames);
+        }
+        System.out.println(allAttributeNames);
         List<String> table1 = tableSelectResultsSeparately.processing(0);
-        //List<String> table2 = tableSelectResultsSeparately.processing(1);
-        indexArray.add(getAttributeNames(tableNames.get(0)));
-//        System.out.println(indexArray.get(0));
-//        System.out.println(getValueOfAttributeName(joinResult.get(0), 0, "DiscID"));
         joinResult = new ArrayList<>();
-
-        joinResult.add(indexArrayToString(indexArray.get(0)));
-
-        List<String> attributeNames = indexArray.get(0);
+        joinResult.add(attributeNamesTableIndexToString(allAttributeNames));
 
         for (int i=0; i<table1.size(); i++) {
-            for (int j=0; j<joinConditions.size(); j++) {
-
-            }
             StringBuilder stringBuilder = new StringBuilder();
-            for (int j=0; j<attributeNames.size(); j++) {
-                stringBuilder.append(getAttributeValueByName(table1.get(i), 0, attributeNames.get(j))).append("#");
+            for (int j=0; j<getAttributeNames(tableNames.get(0)).size(); j++) {
+                stringBuilder.append(getAttributeValueByName(table1.get(i), 0, getAttributeNames(tableNames.get(0)).get(j))).append("#");
             }
             stringBuilder = new StringBuilder(stringBuilder.substring(0, stringBuilder.length()-1));
             joinResult.add(stringBuilder.toString());
@@ -53,7 +52,7 @@ public class Join {
         return joinResult;
     }
 
-    private String indexArrayToString(List<String> list) {
+    private String attributeNamesTableIndexToString(List<String> list) {
         StringBuilder stringBuilder = new StringBuilder();
         for (String i : list) {
             stringBuilder.append(i).append("#");
@@ -62,16 +61,13 @@ public class Join {
     }
 
     private String getAttributeValueByName(String record, int indexOfTable, String attributeName) {
-        return record.split("#")[indexArray.get(indexOfTable).indexOf(attributeName)];
+        return record.split("#")[attributeNamesTableIndex.get(indexOfTable).indexOf(attributeName)];
     }
 
     private List<String> getAttributeNames(String tableName) {
-        List<String> result = databases.getDatabase(Parser.currentDatabaseName).getTable(tableName).getPrimaryKey();
-        List<String> primaryKey = databases.getDatabase(Parser.currentDatabaseName).getTable(tableName).getPrimaryKey();
+        List<String> result = new ArrayList<>();
         for (Attribute i : databases.getDatabase(Parser.currentDatabaseName).getTable(tableName).getStructure()) {
-            if (!primaryKey.contains(i.getName())) {
-                result.add(i.getName());
-            }
+            result.add(i.getName());
         }
         return result;
     }
