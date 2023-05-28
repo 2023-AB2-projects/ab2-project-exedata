459a9c3c34028c093d722c85eadb75a863f8cb1c
Kerekes Balazs-Zoltan <kerekesbaly@gmail.com>
2023-05-28 16:59:37 +0300

    I fixed bug in SelectManager

--- main/src/Backend/SocketServer/Server.java
+++ main/src/Backend/SocketServer/Server.java
@@ -65,7 +65,6 @@ public class Server implements Runnable {
 //                        i=0;
 //                        System.out.println(command);
 //                    }
-
                 }
             } catch (Exception e) {
                 System.out.println(e);
